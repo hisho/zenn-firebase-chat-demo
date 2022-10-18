e332a1893e546349cf0aba8e4cc41c8be5c0922e
hisho <hisho.web@gmail.com>
2022-10-18 19:15:23 +0900

    💄 サインアップページのUIを整えた

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -1,5 +1,42 @@
+import {
+  Box,
+  Button,
+  Center,
+  chakra,
+  Container,
+  FormControl,
+  FormLabel,
+  Grid,
+  Heading,
+  Input,
+  Spacer,
+} from '@chakra-ui/react'
+
 export const Page = () => {
-  return <div>sign up page</div>
+  return (
+    <Container py={14}>
+      <Heading>サインアップ</Heading>
+      <chakra.form>
+        <Spacer height={8} aria-hidden />
+        <Grid gap={4}>
+          <Box display={'contents'}>
+            <FormControl>
+              <FormLabel>メールアドレス</FormLabel>
+              <Input type={'email'} />
+            </FormControl>
+            <FormControl>
+              <FormLabel>パスワード</FormLabel>
+              <Input type={'password'} />
+            </FormControl>
+          </Box>
+        </Grid>
+        <Spacer height={4} aria-hidden />
+        <Center>
+          <Button type={'submit'}>アカウントを作成</Button>
+        </Center>
+      </chakra.form>
+    </Container>
+  )
 }
 
 export default Page
