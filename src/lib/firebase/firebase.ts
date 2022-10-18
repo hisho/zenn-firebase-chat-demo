a33215361da28d9204f6f7cc6bc574dc9b6b4363
hisho <hisho.web@gmail.com>
2022-10-18 18:14:11 +0900

    ✨ firebase.tsを作成し、initializeFirebaseの設定を書いた

---
+++ src/lib/firebase/firebase.ts
@@ -0,0 +1,20 @@
+import { initializeApp } from 'firebase/app'
+import {
+  FIREBASE_API_KEY,
+  FIREBASE_APP_ID,
+  FIREBASE_AUTH_DOMAIN,
+  FIREBASE_MESSAGING_SENDER_ID,
+  FIREBASE_PROJECT_ID,
+  FIREBASE_STORAGE_BUCKET,
+} from '@src/constant/env'
+
+const firebaseConfig = {
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+  appId: FIREBASE_APP_ID,
+}
+
+export const app = initializeApp(firebaseConfig)
