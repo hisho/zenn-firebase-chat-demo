257eba4abb08fae44bc69ef4bcbf994b4a689f9e
hisho <hisho.web@gmail.com>
2022-10-20 03:17:56 +0900

    👽 サインアウトの機能実装

--- src/component/Header/Header.tsx
+++ src/component/Header/Header.tsx
@@ -1,15 +1,29 @@
 import { Button, chakra, Container, Heading } from '@chakra-ui/react'
 import { useAuthContext } from '@src/feature/auth/provider/AuthProvider'
+import { FirebaseError } from '@firebase/util'
+import { getAuth, signOut } from 'firebase/auth'
 
 export const Header = () => {
   const { user } = useAuthContext()
+  const handleSignOut = async () => {
+    try {
+      const auth = getAuth()
+      await signOut(auth)
+    } catch (e) {
+      if (e instanceof FirebaseError) {
+        console.log(e)
+      }
+    }
+  }
 
   return (
     <chakra.header py={4} bgColor={'blue.600'}>
       <Container maxW={'container.lg'}>
         <Heading color={'white'}>
           {user ? (
-            <Button colorScheme={'teal'}>サインアウト</Button>
+            <Button colorScheme={'teal'} onClick={handleSignOut}>
+              サインアウト
+            </Button>
           ) : (
             'ログアウト中'
           )}
