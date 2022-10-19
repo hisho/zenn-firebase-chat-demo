2d47dfb5597101bd60d21de58514f57110c1aa9d
hisho <hisho.web@gmail.com>
2022-10-19 20:25:29 +0900

    ✨ AuthProvをAppで読み込み

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,15 @@
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
 import { initializeFirebaseApp } from '@src/lib/firebase/firebase'
+import { AuthProvider } from '@src/feature/auth/provider/AuthProvider'
 
 initializeFirebaseApp()
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
-      <Component {...pageProps} />
+      <AuthProvider>
+        <Component {...pageProps} />
+      </AuthProvider>
     </ChakraProvider>
   )
 }
