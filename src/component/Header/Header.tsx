e0792a8bcf324b5d1c58222d13ade6bea1141c81
hisho <hisho.web@gmail.com>
2022-10-20 02:45:32 +0900

    💄 サインアウトのUIを実装

--- src/component/Header/Header.tsx
+++ src/component/Header/Header.tsx
@@ -1,4 +1,4 @@
-import { chakra, Container, Heading } from '@chakra-ui/react'
+import { Button, chakra, Container, Heading } from '@chakra-ui/react'
 import { useAuthContext } from '@src/feature/auth/provider/AuthProvider'
 
 export const Header = () => {
@@ -8,7 +8,11 @@ export const Header = () => {
     <chakra.header py={4} bgColor={'blue.600'}>
       <Container maxW={'container.lg'}>
         <Heading color={'white'}>
-          {user ? 'ログイン中' : 'ログアウト中'}
+          {user ? (
+            <Button colorScheme={'teal'}>サインアウト</Button>
+          ) : (
+            'ログアウト中'
+          )}
         </Heading>
       </Container>
     </chakra.header>
