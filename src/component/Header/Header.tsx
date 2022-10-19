7a0cfaf7e83ce1737ae5b73d598ea0c42f8809b7
hisho <hisho.web@gmail.com>
2022-10-19 20:43:18 +0900

    👽 Headerに認証状態を表示するように

--- src/component/Header/Header.tsx
+++ src/component/Header/Header.tsx
@@ -1,10 +1,15 @@
 import { chakra, Container, Heading } from '@chakra-ui/react'
+import { useAuthContext } from '@src/feature/auth/provider/AuthProvider'
 
 export const Header = () => {
+  const { user } = useAuthContext()
+
   return (
     <chakra.header py={4} bgColor={'blue.600'}>
       <Container maxW={'container.lg'}>
-        <Heading color={'white'}>ログアウト中</Heading>
+        <Heading color={'white'}>
+          {user ? 'ログイン中' : 'ログアウト中'}
+        </Heading>
       </Container>
     </chakra.header>
   )
