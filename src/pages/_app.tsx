42e0ffaf2d1dccdb801c294f45330363eda3f52c
hisho <hisho.web@gmail.com>
2022-10-19 20:35:33 +0900

    ✨ HeaderをAppに読み込ませる

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,14 @@ import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
 import { initializeFirebaseApp } from '@src/lib/firebase/firebase'
 import { AuthProvider } from '@src/feature/auth/provider/AuthProvider'
+import { Header } from '@src/component/Header/Header'
 
 initializeFirebaseApp()
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
       <AuthProvider>
+        <Header />
         <Component {...pageProps} />
       </AuthProvider>
     </ChakraProvider>
