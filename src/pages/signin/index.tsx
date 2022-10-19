89b68b80956de1a5f83a5db55d2970e8128ba158
hisho <hisho.web@gmail.com>
2022-10-19 16:46:30 +0900

    ✨ サインインページのフォームの状態を管理した

--- src/pages/signin/index.tsx
+++ src/pages/signin/index.tsx
@@ -11,22 +11,45 @@ import {
   Input,
   Spacer,
 } from '@chakra-ui/react'
+import { FormEvent, useState } from 'react'
 
 export const Page = () => {
+  const [email, setEmail] = useState<string>('')
+  const [password, setPassword] = useState<string>('')
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    console.log({ email, password })
+    e.preventDefault()
+  }
+
   return (
     <Container py={14}>
       <Heading>サインイン</Heading>
-      <chakra.form>
+      <chakra.form onSubmit={handleSubmit}>
         <Spacer height={8} aria-hidden />
         <Grid gap={4}>
           <Box display={'contents'}>
             <FormControl>
               <FormLabel>メールアドレス</FormLabel>
-              <Input type={'email'} />
+              <Input
+                type={'email'}
+                name={'email'}
+                value={email}
+                onChange={(e) => {
+                  setEmail(e.target.value)
+                }}
+              />
             </FormControl>
             <FormControl>
               <FormLabel>パスワード</FormLabel>
-              <Input type={'password'} />
+              <Input
+                type={'password'}
+                name={'password'}
+                value={password}
+                onChange={(e) => {
+                  setPassword(e.target.value)
+                }}
+              />
             </FormControl>
           </Box>
         </Grid>
