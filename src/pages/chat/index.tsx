cf645ade7d5c94e083229f3af098bab99abc0193
hisho <hisho.web@gmail.com>
2022-10-20 17:06:43 +0900

    💄 UIを整えた

--- src/pages/chat/index.tsx
+++ src/pages/chat/index.tsx
@@ -6,14 +6,22 @@ import {
   Input,
   Spacer,
 } from '@chakra-ui/react'
+import { FormEvent, useState } from 'react'
 
 export const Page = () => {
+  const [message, setMessage] = useState<string>('')
+
+  const handleSendMessage = (e: FormEvent<HTMLFormElement>) => {
+    console.log({ message })
+    e.preventDefault()
+  }
+
   return (
     <Container py={14}>
       <Heading>チャット</Heading>
       <Spacer height={8} aria-hidden />
-      <chakra.form display={'flex'} gap={2}>
-        <Input />
+      <chakra.form display={'flex'} gap={2} onSubmit={handleSendMessage}>
+        <Input value={message} onChange={(e) => setMessage(e.target.value)} />
         <Button type={'submit'}>送信</Button>
       </chakra.form>
     </Container>
