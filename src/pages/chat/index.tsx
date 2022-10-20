0a8dd698737b49d8584c9f6fbd65670e4947cab7
hisho <hisho.web@gmail.com>
2022-10-20 16:51:02 +0900

    💄 チャット画面のUIを整えた

--- src/pages/chat/index.tsx
+++ src/pages/chat/index.tsx
@@ -1,5 +1,23 @@
+import {
+  Button,
+  chakra,
+  Container,
+  Heading,
+  Input,
+  Spacer,
+} from '@chakra-ui/react'
+
 export const Page = () => {
-  return <div>chat page</div>
+  return (
+    <Container py={14}>
+      <Heading>チャット</Heading>
+      <Spacer height={8} aria-hidden />
+      <chakra.form display={'flex'} gap={2}>
+        <Input />
+        <Button type={'submit'}>送信</Button>
+      </chakra.form>
+    </Container>
+  )
 }
 
 export default Page
