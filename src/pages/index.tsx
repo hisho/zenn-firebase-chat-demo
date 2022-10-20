82071f95694f6baf212dcf32d16050942f3a1367
hisho <hisho.web@gmail.com>
2022-10-21 04:26:20 +0900

    🔥 トップページからAuthGuardを外した

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,8 @@
 import type { NextPage } from 'next'
 import { Heading } from '@chakra-ui/react'
-import { AuthGuard } from '@src/feature/auth/component/AuthGuard/AuthGuard'
 
 const Page: NextPage = () => {
-  return (
-    <AuthGuard>
-      <Heading>Chakra UI</Heading>
-    </AuthGuard>
-  )
+  return <Heading>Chakra UI</Heading>
 }
 
 export default Page
