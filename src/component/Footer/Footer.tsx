a3e832a8416f6b683f04d1d5f3713a7b42706cfc
hisho <hisho.web@gmail.com>
2022-10-22 07:15:11 +0900

    ✨ Footerにリンクを追加

--- src/component/Footer/Footer.tsx
+++ src/component/Footer/Footer.tsx
@@ -1,9 +1,25 @@
-import { chakra, Container } from '@chakra-ui/react'
+import { chakra, Container, Flex, Link } from '@chakra-ui/react'
+import { Navigate } from '@src/component/Navigate/Navigate'
 
 export const Footer = () => {
   return (
     <chakra.footer py={4} bgColor={'blue.600'} color={'white'}>
-      <Container maxW={'container.lg'}>Footer</Container>
+      <Container maxW={'container.lg'}>
+        <Flex flexDirection={'column'} gap={2} alignItems={'start'}>
+          <Navigate href={(path) => path.$url()}>
+            <Link lineHeight={1}>トップページ</Link>
+          </Navigate>
+          <Navigate href={(path) => path.signin.$url()}>
+            <Link lineHeight={1}>サインイン</Link>
+          </Navigate>
+          <Navigate href={(path) => path.signup.$url()}>
+            <Link lineHeight={1}>サインアップ</Link>
+          </Navigate>
+          <Navigate href={(path) => path.chat.$url()}>
+            <Link lineHeight={1}>チャット</Link>
+          </Navigate>
+        </Flex>
+      </Container>
     </chakra.footer>
   )
 }
